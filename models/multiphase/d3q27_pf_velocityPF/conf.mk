fddba7a46db5e4a9a0667360a77455f95773c36a
hdotyao <hdotyao@gmail.com>
2025-11-12 12:57:06 +1000

    MRT for phase field

---
+++ models/multiphase/d3q27_pf_velocityPF/conf.mk
@@ -0,0 +1,28 @@
+ADJOINT=0
+TEST=FALSE
+OPT="(PFSRT*PFMRT*q27*OutFlow  + BGK + thermo*planarBenchmark)*autosym*geometric*staircaseimp*isograd*tprec"
+# PFMRT - MRT for phase field
+# PFSRT - Using moment space for relaxation but all omega kept same
+# q27 - Q27 lattice structure for phasefield
+#
+# OutFlow - include extra velocity stencil for outflowing boundaries
+#
+# BGK - single relaxation time operator, not really supported- educational purposes only
+#
+# thermo - include energy equation solver for temperature field, influences through
+#        - the surface tension
+#
+# planarBenchmark - thermocapillary benchmark case, must be built with thermo
+#
+# autosym - symmetry boundary conditions
+#
+# geometric - use geometric boundary conditions instead of surface energy, sometimes gives more accurate results
+#
+# staircaseimp - use staircase improvement (applicable to both surface energy and geometric boundary conditions)
+#
+# isograd - use isotopic gradient also near boundaries when calculating phase field gradient, this
+# 	        essentially uses the value of the phase field gradient at the boundary from the previous iteration
+# 	        to calculate the phase field gradient at the boundary in the current iteration
+#
+# tprec - use more precise triangle for interpolation of phase field gradient (only applicable to geometric
+# 	   	  boundary conditions with staircase improvement)
